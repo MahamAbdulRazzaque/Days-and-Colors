7e8e5c6167288a3f3c441cbd5951143de3e3f0a1
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-11-18 17:17:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+// let day = new Date();
+// console.log(day.getDay());
+
+// let date = new Date();
+// console.log(date.getDate());
+
+//getDay....
+// let days = ["sunday" , "monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday"];
+// let now = new Date();
+// let theDay = now.getDay();
+// document.write(theDay , "      Monday");
+
+
+// let days = ["sunday" , "monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday"];
+// let now = new Date().getDay();
+// document.write(days[now]);
+
+
+// let color = ["purple" , "orange" , "yellow" , "red" , "blue" , "skyblue" ,
+//  "pink" , "brown" , "green" , "lightgreen" , "lightbrown" , "black" , "white"];
+// let background = "";
+// for(let i = 0; i < 1; i++){
+//     background += color[Math.floor(Math.random() * color.length)];
+//     document.write(background);
+// }
+// document.body.style.backgroundColor = background;
+
